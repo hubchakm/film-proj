257e4d5b5acb9f83b013058bd4f6a2893fcbddf1
hubchakm <mykolahubchak@gmail.com>
2025-08-25 00:36:00 -0400

    Add proxy and enhance auth UI

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+const filmsServiceUrl = process.env.FILMS_SERVICE_URL || 'http://localhost:3001';
+
 app.use(logger('dev'));
+
+app.use('/api/v1', createProxyMiddleware({
+  target: filmsServiceUrl,
+  changeOrigin: true,
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
